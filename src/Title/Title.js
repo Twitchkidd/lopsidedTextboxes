217a39bae4fcd02225ebd695332b758882bfd118
Gareth Field <field.gareth@gmail.com>
2021-10-20 04:39:07 -0400

    well this is fun

--- src/Title/Title.js
+++ src/Title/Title.js
@@ -1,31 +1,47 @@
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import './Title.css';
+
+const MainText = styled.p`
+	&::after {
+		content: '${props => props.text[props.index - 1]}';
+		color: orange;
+		position: absolute;
+		margin-left: 10vw;
+	}
+`;
 
 export default function Title() {
 	const text = 'Jadefalke';
-	const [index, setIndex] = useState(9);
-	// const [underscore, setUnderscore] = useState('_');
+	const [index, setIndex] = useState(1);
 
-	// useEffect(() => {
-	// 	const id = setInterval(() => {
-	// 		setIndex(val => {
-	// 			if (val > text.length - 2) {
-	// 				setUnderscore(null);
-	// 				clearInterval(id);
-	// 			}
-	// 			return val + 1;
-	// 		});
-	// 	}, Math.random() * 150 + 100);
-
-	// 	return () => clearInterval(id);
-	// }, []);
+	useEffect(() => {
+		// const id = setInterval(() => {
+		// 	setIndex(val => {
+		// 		if (val > text.length - 2) {
+		// 			setUnderscore(null);
+		// 			clearInterval(id);
+		// 		}
+		// 		return val + 1;
+		// 	});
+		// }, Math.random() * 150 + 100);
+		const interval = setInterval(() => {
+			setIndex(val => {
+				if (val === text.length) {
+					clearInterval(interval);
+					return val;
+				}
+				return val + 1;
+			});
+		}, 200);
+		return () => clearInterval(interval);
+	}, []);
 
 	return (
 		<div className='title'>
-			<p id={'main_text'}>
+			<MainText text={text} index={index}>
 				{text.slice(0, index)}
-				{text[index]}
-			</p>
+			</MainText>
 		</div>
 	);
 }
