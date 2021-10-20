823f3b514fd9b3f00cec604f13c15fd34c259534
Gareth Field <field.gareth@gmail.com>
2021-10-20 03:56:00 -0400

    jadefalke's code added

---
+++ tmp.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import './Title.css';
+
+export default function Title() {
+	const text = 'Jadefalke';
+	const [index, setIndex] = useState(0);
+	const [underscore, setUnderscore] = useState('_');
+
+	useEffect(() => {
+		const id = setInterval(() => {
+			setIndex(val => {
+				if (val > text.length - 2) {
+					setUnderscore(null);
+					clearInterval(id);
+				}
+				return val + 1;
+			});
+		}, Math.random() * 150 + 100);
+
+		return () => clearInterval(id);
+	}, []);
+
+	return (
+		<div className='title'>
+			<p id={'main_text'}>
+				{text.slice(0, index)}
+				{text[index]}
+			</p>
+		</div>
+	);
+}
